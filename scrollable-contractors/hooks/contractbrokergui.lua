46f5b0d623cc04d430b2680685870a2b2454c95b
theokrueger <theokrueger@airmail.cc>
2022-04-13 20:55:57 -0400

    add tracker to bottom

--- scrollable-contractors/hooks/contractbrokergui.lua
+++ scrollable-contractors/hooks/contractbrokergui.lua
@@ -1,26 +1,46 @@
--- TODO menu for this
--- TODO better way of scrolling instead of filters hack
---      actually nvm i like that
+-- TODO overcomment code
 
 -- scroll factor, negative value means 'natural' scrolling (ew)
 local scroll_factor = 1
-local shown_buttons = 20
-local head = 1 -- head (top) of shown filter buttons
-local tail -- tail (bottom) of shown filter buttons
+-- n of buttons shown on screen at once
+local shown_buttons = 18
+-- head (top) of shown filter buttons
+local head = 1
+-- tail (bottom) of shown filter buttons
+local tail
+
+-- TODO table
+local previous_panel
+local previous_text
+local previous_right
 
 function ContractBrokerGui.scroll_broker(self, x, y, n)
         -- check if panel exists, is visible, and is hovered over
         if self._panels and self._panels.filters and self._panels.filters:visible() and self._panels.filters:inside(x, y) then
-                head = math.max(1, math.min(#self._filter_buttons, head + n))
+                local button_count = #self._filter_buttons
+
+                head = math.max(1, math.min(button_count, head + n))
                 tail = head + shown_buttons
                 local y = 0
+
                 for i, panel in ipairs(self._filter_buttons) do
-                        if i < head or i > tail then
+                        if i == tail and tail ~= button_count and panel:child('text') then
+                                if previous_panel and previous_text then
+                                        self._filter_buttons[previous_panel]:child('text'):set_text(previous_text)
+                                end
+                                -- TODO functions n stuff
+                                previous_panel = i
+                                previous_text = panel:child('text'):text()
+                                panel:child('text'):set_text(head ..'-'.. tail .. ' of ' .. button_count)
+                                panel:set_righttop(self._panels.filters:w() - 4, y)
+                                panel:set_visible(true)
+
+                        elseif i < head or i > tail then
                                 panel:set_visible(false)
                                 panel:set_righttop(self._panels.filters:w() - 4, 0)
                         else
                                 panel:set_righttop(self._panels.filters:w() - 4, y)
-                                y = panel:bottom() + 4 
+                                y = panel:bottom() + 4
                                 panel:set_visible(true)
                         end
                 end
@@ -28,7 +48,21 @@ function ContractBrokerGui.scroll_broker(self, x, y, n)
         end
 end
 
-
+--[[ update initial layout
+-- TODO BROKEN
+Hooks:PostHook(
+        ContractBrokerGui,
+        'init',
+        'SCB_init',
+        function(self, ws, fullscreen_ws, node)
+                if self._panels and self._panels.filters then
+                        previous_panel = shown_buttons + 1
+                        previous_text = self._filter_buttons[previous_panel]:child('text'):text()
+                        self._filter_buttons[previous_panel]:child('text'):set_text(1 ..'-'.. shown_buttons .. ' of ' .. #self._filter_buttons)
+                end
+        end
+)
+]]--
 -- scrolling hooks
 Hooks:PostHook(
         ContractBrokerGui,
