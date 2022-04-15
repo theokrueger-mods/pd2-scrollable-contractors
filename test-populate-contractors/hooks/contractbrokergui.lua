5b51bdd96d9d4b4ad2c572d98c073a8fc617d56d
theokrueger <theokrueger@airmail.cc>
2022-04-14 22:02:31 -0400

    lazily improve test

--- test-populate-contractors/hooks/contractbrokergui.lua
+++ test-populate-contractors/hooks/contractbrokergui.lua
@@ -1,3 +1,40 @@
+local tbl = {
+        "bain",
+        "the elephant",
+        "the continental",
+        "vlad",
+        "hector",
+        "locke",
+        "asian man",
+        "oruga",
+        "Oreztov",
+        "crashbreaker",
+        "hoppip",
+        "fish",
+        "mercu",
+        "offyerrocker",
+        "rex",
+        "lofi",
+        "almir",
+        "matthelzor",
+        "mako",
+        "shovel man",
+        "alan turing (real)",
+        "paydayfan1234",
+        "beardlibhater",
+        "simon",
+        "p3dev",
+        "luajit itself",
+        "a racial slur",
+        "the super mario brothers",
+        "i am running out of names",
+        "balls",
+        "test1",
+        "test2",
+        "sora",
+        "joe mama"
+}
+
 -- copied function with removal of allow_contact so you get one filter per heist lmfao
 function ContractBrokerGui:_setup_filter_contact()
 	local contacts = {}
@@ -12,7 +49,7 @@ function ContractBrokerGui:_setup_filter_contact()
 			local allow_contact = true
 			--allow_contact = not table.contains(contacts, contact) and (not contact_tweak or not contact_tweak.hidden)
 			if allow_contact then
-				table.insert(contacts, contact)
+				table.insert(contacts, tbl[math.random(#tbl)])
 				table.insert(filters, {
 					id = contact,
 					data = contact_tweak
@@ -21,9 +58,9 @@ function ContractBrokerGui:_setup_filter_contact()
 		end
 	end
 
-	table.sort(filters, function (a, b)
+	--[[table.sort(filters, function (a, b)
 		return managers.localization:to_upper_text(a.data.name_id) < managers.localization:to_upper_text(b.data.name_id)
-	end)
+                end)]]--
 
 	local last_y = 0
 	local check_new_job_data = {
@@ -33,7 +70,7 @@ function ContractBrokerGui:_setup_filter_contact()
 
 	for filter_index, contact in ipairs(filters) do
 		check_new_job_data.filter_param = contact
-		local text = self:_add_filter_button(contact.data.name_id, last_y, {
+		local text = self:_add_filter_button(tbl[math.random(#tbl)], last_y, {
 			check_new_job_data = check_new_job_data
 		})
 		last_y = text:bottom() + 1
