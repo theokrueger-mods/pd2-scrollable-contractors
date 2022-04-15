7e78021ecfe947766d760acc20ffa5ac183baeab
theokrueger <theokrueger@airmail.cc>
2022-04-15 12:07:27 -0400

    overcomment code

--- scrollable-contractors/hooks/contractbrokergui.lua
+++ scrollable-contractors/hooks/contractbrokergui.lua
@@ -1,5 +1,3 @@
--- TODO overcomment code
-
 -- scroll factor, negative value means 'natural' scrolling (ew)
 local scroll_factor = 1
 -- n of buttons shown on screen at once
@@ -9,7 +7,12 @@ local head = 1
 -- tail (bottom) of shown filter buttons
 local tail
 
--- TODO table
+-- previous data for overwritten stuff
+local previous_data = {
+        panel = nil,
+        text = nil,
+        pos = nil
+}
 local previous_panel
 local previous_text
 local previous_right
@@ -17,30 +20,58 @@ local previous_right
 function ContractBrokerGui.scroll_broker(self, x, y, n)
         -- check if panel exists, is visible, and is hovered over
         if self._panels and self._panels.filters and self._panels.filters:visible() and self._panels.filters:inside(x, y) then
+                -- total amount of contractors
                 local button_count = #self._filter_buttons
 
+                -- clamp head and tail to total buttons
                 head = math.max(1, math.min(button_count, head + n))
-                tail = head + shown_buttons
+                tail = math.min(head + shown_buttons, button_count)
+
+		-- starting position of broker buttons
                 local y = 0
 
+		-- iterate over every broker button
                 for i, panel in ipairs(self._filter_buttons) do
-                        if i == tail and tail ~= button_count and panel:child('text') then
-                                if previous_panel and previous_text then
-                                        self._filter_buttons[previous_panel]:child('text'):set_text(previous_text)
+                        -- test if this will be the last shown entry
+                        if i == tail then
+                                -- revert overridden text to previous state
+                                if previous_data.panel and previous_data.text then
+                                        self._filter_buttons[previous_data.panel]:child('text'):set_text(previous_data.text)
                                 end
-                                -- TODO functions n stuff
-                                previous_panel = i
-                                previous_text = panel:child('text'):text()
-                                panel:child('text'):set_text(head ..'-'.. tail .. ' of ' .. button_count)
-                                panel:set_righttop(self._panels.filters:w() - 4, y)
-                                panel:set_visible(true)
 
+				local text_panel = panel:child('text')
+				-- store new previous data for next scroll
+				-- there might be wasted cycles here because if this is the last entry we will not be using this data and overwriting the same panel which we never even modified, but it does save lines of code
+                                previous_data.panel = i
+                                previous_data.text = text_panel:text()
+
+                                -- if this is the last entry in the whole list we want its original text to be shwon
+				if tail ~= button_count then
+                                        -- set current panel text to "<head> - <tail> of <total>"
+					-- ex: "3 - 21 of 36"
+                                        text_panel:set_text(head ..'-'.. tail .. ' of ' .. button_count)
+					
+					-- positions panel where it should be
+                                        panel:set_righttop(self._panels.filters:w() - 4, y)
+
+					-- show our panel in case it is hidden
+                                        panel:set_visible(true)
+                                end
+                        -- checks if this panel should not be shown ...
                         elseif i < head or i > tail then
+                                -- ... and then hides it!
                                 panel:set_visible(false)
+
+                                -- sets position to top (hidden) because it should be hidden
                                 panel:set_righttop(self._panels.filters:w() - 4, 0)
+
+                        -- and if this panel meets no other criteria, it means it should be shown
                         else
+                                -- so we set its position to below the previous panel
                                 panel:set_righttop(self._panels.filters:w() - 4, y)
+                                -- and tell the next panel where the bottom of it is
                                 y = panel:bottom() + 4
+                                -- and shows itself
                                 panel:set_visible(true)
                         end
                 end
